151fcccc24d0f6d3333d80809e165c4843046c53
Markus Landgraf <Markus.Landgraf@me.com>
2015-05-31 17:52:02 +0200

    Revert ad3bed0..144f3ff
    This rolls back to commit ad3bed04c8eed8649e1738b7c12204650d83c747.

--- src/frames.jl
+++ src/frames.jl
@@ -6,7 +6,6 @@
 # t::FloatingPoint - epoch in MJD2000
 #OUTPUT
 # rv::Vector - state vector in [DU, DU/TU]
-# Written by Markus Landgraf 30Jun15
 #------------------------------------------------------------------------
 function eci2emrotpulse(rv::Vector, t::FloatingPoint)
     sM=jpleph(t,"Moon","Earth");
@@ -32,16 +31,8 @@ function eci2emrotpulse(rv::Vector, t::FloatingPoint)
     return [npos;nvel];
 end
 
-#------------------------------------------------------------------------
 #transform state vector from Earth-Moon rotating pulsating frame 
 #[natural units] to Earth centric inertial MEE200 frame
-#INPUT 
-# rv::Vector - state vector in [DU, DU/TU]
-# t::FloatingPoint - epoch in MJD2000
-#OUTPUT
-# rv::Vector - state vector in [km, km/s]
-# Written by Markus Landgraf 30Jun15
-#------------------------------------------------------------------------
 function emrotpulse2eci(rv::Vector, t::FloatingPoint)
     sM=jpleph(t,"Moon","Earth");
     rM=norm(sM[1:3]);
